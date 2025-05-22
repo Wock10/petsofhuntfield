157bde61ae5080c190e1b67c9b31a2dd3b370932
Wock10 <98241634+Wock10@users.noreply.github.com>
2025-05-22 15:05:44 -0400

    fix: enforce S3 v4 signing

--- api/uploadImages.js
+++ api/uploadImages.js
@@ -5,6 +5,7 @@ const s3 = new AWS.S3({
   region: process.env.AWS_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  signatureVersion: 'v4',
 });
 
 const BUCKET = process.env.S3_BUCKET_NAME;
